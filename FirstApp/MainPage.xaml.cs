785f7aac5eda69963dfbe3750719009794a3e825
Anders <anders.bjoerk.unf@gmail.com>
2025-04-04 00:06:17 +0200

    [ADD] Picker

--- FirstApp/MainPage.xaml.cs
+++ FirstApp/MainPage.xaml.cs
@@ -2,12 +2,25 @@
 
 public partial class MainPage : ContentPage
 {
-
+	List<string> itemList = new List<string>();
 	public MainPage()
 	{
 		InitializeComponent();
+
+
+		itemList.Add("Baboon");
+		itemList.Add("Capuchin Monkey");
+		itemList.Add("Blue monkey");
+		itemList.Add("Squirrel monkey");
+		itemList.Add("Golden Lion Tamarin");
+		itemList.Add("Howler Monkey");
+		itemList.Add("Japanese Macaque");
+
+		Picker picker = new Picker { Title = "Select a monkey" };
+		picker.ItemsSource = itemList; 
 	}
 
+	
 
 
 }
