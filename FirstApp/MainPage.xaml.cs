92708b792d0092a7e9cc6e1fc511c048c22114ee
Anders <anders.bjoerk.unf@gmail.com>
2025-10-09 18:23:20 +0200

    [ADD] Added new button and a text box

--- FirstApp/MainPage.xaml.cs
+++ FirstApp/MainPage.xaml.cs
@@ -1,38 +1,41 @@
-﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.Internals;
-
+﻿
 namespace FirstApp;
 
 public partial class MainPage : ContentPage
 {
 	private int count = 0;
-
-
 	public MainPage()
 	{
 		Console.WriteLine("Initializing MainPage");
 
 		InitializeComponent();
-
-
 	}
 
 	public void ChangeLabel()
 	{
-		count++;
 		IncShower.Text = count.ToString();
 	}
 
-	void OnButtonClicked(object sender, EventArgs e)
+	void IncrementButton(object sender, EventArgs e)
 	{
+		count++;
+		ChangeLabel();
+	}
+
+	void DecrementButton(object sender, EventArgs e)
+	{
+		count--;
 		ChangeLabel();
 	}
 
 	async void OnPickerSelected(object sender, EventArgs e)
 	{
-		await DisplayAlert("Selected", ((Picker)sender).SelectedItem.ToString(), "OK");
+		await DisplayAlert("Alert", "You selected " + picker.SelectedItem, "OK");
+	}
+
+	async void EnterEntry(object sender, EventArgs e)
+	{
+		await DisplayAlert("Alert", "You entered " + entry.Text, "OK");
 	}
 
 }
