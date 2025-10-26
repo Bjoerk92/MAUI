85bac1303ed2254ba1897af17d861314c8d0b377
Anders <anders.bjoerk.unf@gmail.com>
2025-10-26 10:09:37 +0100

    [FIX] change the default startup size of the window

--- FirstApp/App.xaml.cs
+++ FirstApp/App.xaml.cs
@@ -9,6 +9,16 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new AppShell());
+		const int newWidth = 1200;
+		const int newHeight = 800;
+
+		var newWindow = new Window(new AppShell())
+		{
+			Width = newWidth,
+			Height = newHeight
+		};
+
+
+		return newWindow;
 	}
 }
